7956fa080eaead75adc24b47d3e7ffc2e6cf21ec
Prashant Dodiya <pra17dod@gmail.com>
2020-09-06 01:27:30 +0530

    add posts

--- helppeople/helppeople/settings.py
+++ helppeople/helppeople/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '-iwuphi&$x7-d5!m+kroc9x4re2up7l8#0l$fhzc^j__6ri'
+
 from my_secrets import secrets
 SECRET_KEY = secrets.SECRET_KEY
 
