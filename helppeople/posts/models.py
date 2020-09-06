5162afe580099ef60cf961ae5da746fc7a983935
Prashant Dodiya <pra17dod@gmail.com>
2020-09-06 13:42:56 +0530

    add database

--- helppeople/posts/models.py
+++ helppeople/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=250)
+    img= models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    people = models.IntegerField()
+    conctact = models.TextField()
+
