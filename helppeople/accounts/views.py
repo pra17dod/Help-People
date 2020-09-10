cd8f4e44778bcf408fea111b22016309951990b9
Prashant Dodiya <pra17dod@gmail.com>
2020-09-10 20:22:13 +0530

    Add page navigation

--- helppeople/accounts/views.py
+++ helppeople/accounts/views.py
@@ -1,52 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
-# from .forms import UserRegisterForm
 
-# Create your views here.
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserRegisterForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             messages.success(request, f'Account created for {username}!')
-#             return redirect('index.html')
-#     else:
-#         form = UserRegisterForm()
-#     return render(request, 'signup.html', {'form': form})
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
+        if User.objects.filter(username=username).exists():
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                messages.info(request,'Welcome!')
+                return redirect('/')
+            else:
+                messages.info(request, 'Invalid username or password')
+                return redirect('signin')
+        else:
+            messages.info(request, 'User not found, signup first!')
+            return redirect('signup')
 
-
-
-
-# def signin(request):
-#     return render(request, 'signin.html')
-
-# def signup(request):
-#     if request.method == "POST":
-#         username = request.POST['username']
-#         first_name = request.POST['first_name']
-#         last_name = request.POST['last_name']
-#         email = request.POST['email']
-#         phone = request.POST['phone']
-#         password1 = request.POST['password']
-#         confirm_password = request.POST['confirm_password']
-        
-#         user = User.objects.create_user(username=username, email=email, password=confirm_password)
-#         user.save()
-#         print('user created')
-#         return redirect('/')
-
-#     else:        
-#         return render(request, 'signup.html')
-
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.models import User, auth
-# from django.shortcuts import render
-# from django.contrib import messages
-
-# # Create your views here.
+    else:
+        return render (request, 'signin.html')
 
 def signup(request):
 
@@ -75,9 +50,8 @@ def signup(request):
             return redirect('signup')  
     else:    
         return render(request,'signup.html')
- 
 
-
-
-def signin(request):
-    return render(request, 'signin.html')
+def signout(request):
+    auth.logout(request)
+    messages.info(request, "Thank you! See you soon")
+    return redirect('/')
